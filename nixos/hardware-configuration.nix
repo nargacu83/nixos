2ef9b4f4c7282c149c721982f593e92061d9029b
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2023-06-25 19:49:30 +0200

    by label?

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -1,7 +1,7 @@
 # This is just an example, you should generate yours with nixos-generate-config and put it in here.
 {
   fileSystems."/" = {
-    device = "/dev/vda1";
+    device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
   };
 
