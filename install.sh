1de5800a54426f7771add3b896cd0ab858cb7c12
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2023-06-27 21:35:28 +0200

    Auto copy generated hardware configuration

--- install.sh
+++ install.sh
@@ -58,4 +58,5 @@ mount_file_system
 echo " >> Installing NixOS configuration"
 sleep 1
 nixos-generate-config --root /mnt
+cp /mnt/etc/nixos/hardware-configuration.nix nixos/hardware-configuration.nix
 nixos-install --flake .#nixos
