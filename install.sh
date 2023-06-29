8a8f2748e4eeac4e48f7620507341b1d43679226
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2023-06-29 12:57:06 +0200

    Removed impure

--- install.sh
+++ install.sh
@@ -59,4 +59,4 @@ echo " >> Installing NixOS configuration"
 sleep 1
 nixos-generate-config --root /mnt
 cp /mnt/etc/nixos/hardware-configuration.nix nixos/hardware-configuration.nix
-nixos-install --root /mnt --impure --flake .#nixos
+nixos-install --root /mnt --flake .#nixos
