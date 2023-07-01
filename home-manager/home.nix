be5024332fa180d4630803ba13800c94399fc8f6
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2023-07-01 23:37:23 +0200

    Added git credentials

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -119,7 +119,11 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    userName  = "Quentin Franchi";
+    userEmail = "dev.quentinfranchi@protonmail.com";
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
