b5f9d0f1918e49049103916b17499db8ff1296b5
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2023-08-25 21:04:46 +0200

    Added GDM

--- nixos/lightdm/default.nix
+++ nixos/lightdm/default.nix
@@ -1,25 +1,28 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.xserver.displayManager.lightdm = {
+  services.xserver.displayManager.gdm = {
       enable = true;
-      background = ./background.png;
-
-      greeters.gtk = {
-        enable = true;
-        cursorTheme.name = "Adwaita";
-        iconTheme = {
-          name = "Dracula";
-          package = pkgs.dracula-icon-theme;
-        };
-        theme = {
-          name = "Dracula";
-          package = pkgs.dracula-theme;
-        };
-        indicators = [ "~session" "~power" ];
-        extraConfig = ''
-          hide-user-image = true
-        '';
-      };
   };
+  # services.xserver.displayManager.lightdm = {
+  #     enable = true;
+  #     background = ./background.png;
+
+  #     greeters.gtk = {
+  #       enable = true;
+  #       cursorTheme.name = "Adwaita";
+  #       iconTheme = {
+  #         name = "Dracula";
+  #         package = pkgs.dracula-icon-theme;
+  #       };
+  #       theme = {
+  #         name = "Dracula";
+  #         package = pkgs.dracula-theme;
+  #       };
+  #       indicators = [ "~session" "~power" ];
+  #       extraConfig = ''
+  #         hide-user-image = true
+  #       '';
+  #     };
+  # };
 }
