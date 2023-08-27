9871d94fed30e5dbea9b873bed4ffef340212ef7
Quentin Franchi <dev.quentinfranchi@protonmail.com>
2023-08-27 18:17:46 +0200

    Improved flatpacks install scripts

--- install-flatpaks.sh
+++ install-flatpaks.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env sh
 
-FLATPAKS=(
+flatpacks=(
    "com.github.tchx84.Flatseal"
    "org.gtk.Gtk3theme.adw-gtk3-dark"
    "io.freetubeapp.FreeTube"
-   "com.obsproject.Studio"
 
    "info.beyondallreason.bar"
    "com.valvesoftware.Steam"
    "org.freedesktop.Platform.VulkanLayer.gamescope"
 
    "org.godotengine.Godot"
-   "com.unity.UnityHub"
    "io.github.achetagames.epic_asset_manager"
 
+   "com.discordapp.Discord"
+)
+
+gtk_theme="adw-gtk3-dark"
+
+# Flatpaks to apply the theme
+flatpacks_theme=(
+   "io.freetubeapp.FreeTube"
+   "info.beyondallreason.bar"
+   "com.valvesoftware.Steam"
+   "org.godotengine.Godot"
    "com.discordapp.Discord"
 )
 
@@ -31,12 +40,27 @@ function print_error() {
 }
 
 print_message "Installing Flatpaks..."
-for flatpak in "${FLATPAKS[@]}"; do
+for flatpak in "${flatpaks[@]}"; do
     flatpak install -y $flatpak || print_error "Could not install flatpak: $flatpak"
 done
 
 print_message "Configuring Flatpaks..."
-flatpak --user override --filesystem=/home/$USER/.icons/:ro
+flatpak --user override --reset
+
+# Fixes for fonts and icons
+ln -s /run/current-system/sw/share/X11/fonts ~/.local/share/fonts
+flatpak --user override --filesystem=$HOME/.local/share/fonts:ro
+flatpak --user override --filesystem=$HOME/.icons/:ro
 flatpak --user override --filesystem=/usr/share/icons/:ro
 flatpak --user override --filesystem=/nix/store:ro
 
+## Only for Discord
+flatpak --user override --unset-env=XCURSOR_PATH
+##
+
+print_message "Applying theme..."
+for flatpak in "${flatpacks_theme[@]}"; do
+    flatpak --user override --env=GTK_THEME=$gtk_theme $flatpak
+done
+
+print_message "Flatpaks installed and configured."
